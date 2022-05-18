418c80188a9c6524ec7a8bd7e6c98963c29953b4
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-05-18 11:15:11 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
-import styled from 'styled-components'
-import luffy from './assets/img/luffy.png'
-import backgroundImg from './assets/img/sky.jpg'
-import './App.css';
+import styled from "styled-components";
+import luffy from "./assets/img/luffy.png";
+import skyImg from "./assets/img/sky.jpg";
+import { quotes } from "./components";
 
-function App() {
+export default function App() {
   return (
     <Content /*className="App"*/>
-      <p role="p">Text One Piece</p>
-      <button>Quote One Piece</button>
-      <img src={luffy} alt="Luffy with straw hat" />
+      <quotes quote={"ok"} speaker={"speaker"} />
+      <LuffyImg src={luffy} alt="Luffy with straw hat" />
     </Content>
   );
 }
 
-
-const Content =styled.div`
-  height: 100vh;
+const Content = styled.div`
+  height: 90vh;
   padding: 0 2rem;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  align-items: flex-end;
+  background: url(${skyImg}) no-repeat center center;
 `;
 
+/* const DivText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+  width: 50%;
+`;*/
 
-export default App
+const LuffyImg = styled.img`
+  width: 50%;
+  height: auto;
+`;
